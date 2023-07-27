b773089a49387061aa5bbe485ee00484e4faa7e0
Baqar Ali <sayedalintl@gmail.com>
2023-07-27 23:59:21 +0530

    <Hello>

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Button({product}) {
   return (
+    <section>
+  aasssaa
     <button  className="bg-cyan-500 w-28 text-xl h-17 text-white hover:bg-cyan-600 ...">Add to Cart</button>
+    </section>
+    
   )
 }
